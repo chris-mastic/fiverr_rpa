f790625351f9e1a5cff5ae7e214b4f90fe6ba483
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-05-29 11:45:51 -0500

    initial POC

--- search_webpage.py
+++ search_webpage.py
@@ -1,7 +1,7 @@
 import pyautogui
-import cv2
 import screeninfo
 import time
+from selenium import webdriver
 
 # Move mouse to upper-left corner to abort program
 pyautogui.FAILSAFE = True
@@ -19,24 +19,43 @@ for screen in screens:
         break
 print(main_screen)
 
-png_list = ['484.png', '4841.png', '4842.png', '48411.png', '484110.png', '484121.png', '484230.png', '492110.png']
+codes = ['484', '4841', '4842', '48411', '484110', '484121', '484230', '492110']
 # png_list = ['484.png']
 # result = pyautogui.locateOnScreen('484.png')
 # print(f"result{result}")
-for png in png_list:
-    image_position = pyautogui.locateOnScreen('static/images/' + png, confidence=0.8)
+time.sleep(4)
+for code in codes:
+    image_position = pyautogui.locateOnScreen('static/images/naics.png', confidence=0.7)
+    
     if image_position:
         # Get the center coordinates
         x, y = pyautogui.center(image_position)
-
-        # Move the mouse to the center
-        pyautogui.moveTo(x, y)
-        print(f"x {x} and y {y} name= {png}")
+        print(f"NAICS found at {x}, {y}")
+        pyautogui.moveTo(x + 200, y)
         pyautogui.click()
-        pyautogui.hotkey('ctrl', 'shift', 'k')
+        # clear text
+        for x in range(10):
+            pyautogui.press('delete')
+        for x in range(10):
+            pyautogui.press('backspace')
+        # for x in range(100):
+        # for char in code:
+        #     pyautogui.press('backspace')
         
-        # Click to focus on the textbox
-        # pyautogui.click()
+        time.sleep(5)
+        pyautogui.typewrite(code, interval=0.2)
+        #time.sleep(5) 
+        search_btn_image = pyautogui.locateOnScreen('static/images/search.png', confidence=0.7)
+        time.sleep(5)
+        srch_x, srch_y = pyautogui.center(search_btn_image)
+        pyautogui.moveTo(srch_x, srch_y)
+        pyautogui.click()
+        time.sleep(5) 
+
+        #duplicate tab
+        pyautogui.hotkey('alt','shift', 'd') 
+ 
+
     else:
         print("Image not found.")
-    time.sleep(10)
+    time.sleep(2)
