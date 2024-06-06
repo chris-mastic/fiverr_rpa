f871228eb4b3f0bf57dced5dd2769c88e5af0085
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2024-06-06 12:02:51 -0500

    work on bot not seeing images. add logging

--- search_webpage.py
+++ search_webpage.py
@@ -25,55 +25,57 @@ print(main_screen)
 userprofile = os.environ['USERPROFILE']
 codes_path = os.path.join(userprofile,'Documents\search_codes.txt')
 
-log_msg = ""
+codes = []
+with open(codes_path, 'r') as file:
+    for line in file:
+        codes.append(line.strip())
 
-with open(os.path.join(userprofile,'Documents\search_webpage.log'), 'a') as file:
+try:
+    iteration_counter = 0
+    file = open(os.path.join(userprofile,'Documents\search_webpage.log'), 'a') 
     file.write(f"{datetime.datetime.now()}\n")
 
-    codes = []
-    with open(codes_path, 'r') as file:
-        for line in file:
-            codes.append(line.strip())
-
-    try:
-        iteration_counter = 0
-        time.sleep(4)
-        for code in codes:
-            log_msg + code + '\n'
-            image_position = pyautogui.locateOnScreen('naics.png', confidence=0.7)
-            log_msg + image_position + '\n'
-            print(f"code {code}")
-            iteration_counter += 1
-            if image_position:
-                # Get the center coordinates
-                x, y = pyautogui.center(image_position)
-                file.write(f"NAICS found at {x}, {y}\n")
-                log_msg + "NAICS found at " + x + ' ' + y +'\n'
-                pyautogui.moveTo(x + 200, y)
-                pyautogui.click()
-                # clear text
-                for x in range(10):
-                    pyautogui.press('delete')
-                for x in range(10):
-                    pyautogui.press('backspace')
+    time.sleep(4)
+    for code in codes:
+        file.write(f'searching for NAICS.png...\n')
+        image_position = pyautogui.locateCenterOnScreen('arrow.png', confidence=0.7)
+        file.write(f'image_position {image_position}\n')
+        print(f"code {code}")
+        iteration_counter += 1
+        if image_position:
+            # Get the center coordinates
+            #x, y = pyautogui.center(image_position)
+            print("in if ")
+            file.write(f"NAICS found at{image_position[0]}\n")
+            pyautogui.moveTo(image_position[0] + 50, image_position[1])
+            pyautogui.click()
+            # clear text
+            for x in range(10):
+                pyautogui.press('delete')
+            for x in range(10):
+                pyautogui.press('backspace')
             
-                time.sleep(1)
-                pyautogui.typewrite(code, interval=0.2)
-                search_btn_image = pyautogui.locateOnScreen('search.png', confidence=0.7)
-                time.sleep(2)
-                file.write(f"srch_x {srch_x} and srch_y {srch_y}\n")
-                log_msg + 'srch_x ' + srch_x + ' ' + 'and srch_y ' + srch_y + '\n'
-                srch_x, srch_y = pyautogui.center(search_btn_image)
-                pyautogui.moveTo(srch_x, srch_y)
-                pyautogui.click()
-                time.sleep(2) 
+            time.sleep(1)
+            pyautogui.typewrite(code, interval=0.2)
+            file.write("Searching for serach.png...")
+            search = pyautogui.locateCenterOnScreen('search.png')
+            pyautogui.moveTo(search)
+            # search_btn_image = pyautogui.locateOnScreen('search.png', confidence=0.7)
+            # time.sleep(2)
+            # file.write(f"srch_x {srch_x} and srch_y {srch_y}\n")
+            # srch_x, srch_y = pyautogui.center(search_btn_image)
+            # pyautogui.moveTo(srch_x, srch_y)
+            pyautogui.click()
+            time.sleep(2) 
             
-                if iteration_counter != len(codes):
-                    pyautogui.hotkey('alt','shift', 'd') 
+            if iteration_counter != len(codes):
+                pyautogui.hotkey('alt','shift', 'd') 
         
 
                                  
-    except:
-        log_msg + "ERROR: Image not found \n"
-        time.sleep(2)
-    file.write(log_msg)
+except BaseException as e: 
+    file.write(f'ValueError {e.with_traceback}\n')
+    time.sleep(2)
+finally:
+    file.close()
+    
